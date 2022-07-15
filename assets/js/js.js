4fffe41ed97ac393501833613723638060734ead
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-14 21:01:54 -0400

    created date format for js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,1 +1,8 @@
+//Current date format
+let date = new Date ()
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
 
+let fullDate= '${day}.${month}.${year}.';
+console.log(fullDate);
