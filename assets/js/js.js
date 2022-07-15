0676452ce8cc151e7e12c2d8b71913d0aba6e010
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-14 22:59:21 -0400

    organize JS file

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,63 +1,65 @@
 $(document).ready(function() {
 
-//Current date format
-var cTime = moment().format("dddd,MMMM, YYYY")
-$("currentDay").append(cTime)
-
-//curent time
-var businessHoursMil = [
-    "09",
-    "10",
-    "11",
-    "12",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-]
-var milHour = moment().format("HH")
-
-//current hour 
-for (var i = 0; i < businessHoursMil.length; i++) {
-    if (milHour == businessHoursMil[i]) {
+    //Current date format
+    var cTime = moment().format("dddd,MMMM, YYYY")
+    $("currentDay").append(cTime)
+  
+    //curent time
+    var businessHoursMil = [
+      "09",
+      "10",
+      "11",
+      "12",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+    ]
+    var milHour = moment().format("HH")
+  
+    //current hour 
+    for (var i = 0; i < businessHoursMil.length; i++) {
+      if (milHour == businessHoursMil[i]) {
         var updateClass = $("." + businessHoursMil[i]);
         updateClass.addCLass("present");
         $("." + businessHoursMil[i]).append(updateClass)
+      }
     }
-}
-
-//past hours
-for (var i = 0; i < businessHoursMil.length; i++) {
-    if (milHour > businessHoursMil[i]) {
+  
+    //past hours
+    for (var i = 0; i < businessHoursMil.length; i++) {
+      if (milHour > businessHoursMil[i]) {
         var updateClass = $("." + businessHoursMil[i]);
         updateClass.addCLass("present");
         $("." + businessHoursMil[i]).append(updateClass)
+      }
     }
-}
-
-//Coming hours
-for (var i = 0; i < businessHoursMil.length; i++) {
-    if (milHour < businessHoursMil[i]) {
+  
+    //Coming hours
+    for (var i = 0; i < businessHoursMil.length; i++) {
+      if (milHour < businessHoursMil[i]) {
         var updateClass = $("." + businessHoursMil[i]);
         updateClass.addCLass("present");
         $("." + businessHoursMil[i]).append(updateClass)
+      }
     }
-}
-
-//Save Entry
-function loadEntry(key) {
-    let savedEntry = localStorage.getItem(key);
-    if (savedEntry) {
+  
+    //Save Entry
+    function loadEntry(key) {
+      let savedEntry = localStorage.getItem(key);
+      if (savedEntry) {
         $('#col-text-' + key).text(savedEntry);
-    }
-};
-//Save Button
-$('.saveBtn').on('click', function(event) {
-    event.preventDefault();
-
-    let entryId = parseInt($(this).closest('.time-block').attr('id'));
-    let newEntry = $.trim($(this).parent().find('textarea').val());
-    workDay[entryId].event = newEntry;
-    localStorage.setItem(entryId, newEntry);
-})})
+      }
+    };
+    //Save Button
+    $('.saveBtn').on('click', function(event) {
+      event.preventDefault();
+  
+      let entryId = parseInt($(this).closest('.time-block').attr('id'));
+      let newEntry = $.trim($(this).parent().find('textarea').val());
+      workDay[entryId].event = newEntry;
+      localStorage.setItem(entryId, newEntry);
+    })
+  })
+  
