247cff8e4414938e055b7ffb25c1844a4c95db07
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-14 21:26:30 -0400

    created hours format

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,5 +5,18 @@ var cTime= moment().format("dddd,MMMM, YYYY")
 $("currentDay").append(cTime)
 
 //curent time
+var businessHoursMil = [
+    "09",
+    "10",
+    "11",
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+]
+var milHour = moment().format("HH")
 
+//Save Button
 
